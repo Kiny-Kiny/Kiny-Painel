4b2bc90ed3d43301341038b1ec87650d44a5f93a
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-03-14 21:31:24 -0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -246,50 +246,23 @@ def gerar_pessoa(token): #####REWORK
         gerar_pessoa(token)
 
 def consultaplaca():
-    #http://api.masterplaca.devplank.com/v2/placa/{placa}/json
-            os.system("figlet KINY")
-            print(f'{C}[{G}i]{C}Digite o numero da placa.')
-            placa_input = input("===>")
-            req = requests.get('https://apicarros.com/v1/consulta/{}/json'.format(placa_input), verify = False) # JSQ7436
-            placa_data = req.json()
-            clear()
-            os.system('figlet KINY')
-            try:
-                if (placa_data['codigoRetorno']) == "0":
-                    print(f"{C}Ano: {B}{placa_data['ano']}{C}")
-                    print(f"Data: {B}{placa_data['data']}{C}")
-                    print(f"Modelo: {B}{placa_data['modelo']}{C}")
-                    print(f"Ano do modelo: {B}{placa_data['anoModelo']}{C}")
-                    print(f"Cor: {B}{placa_data['cor']}{C}")
-                    print(f"Marca: {B}{placa_data['marca']}{C}")
-                    print(f"Roubo/furto: {B}{placa_data['dataAtualizacaoRouboFurto']}{C}")
-                    print(f"Situação: {B}{placa_data['situacao']}{C}")
-                    print(f"Placa: {B}{placa_data['placa']}{C}")
-                    print(f"Chassi: {B}{placa_data['chassi']}{C}")
-                    print(f"UF: {B}{placa_data['uf']}{C}")
-                    print(f"Município: {B}{placa_data['municipio']}{C}")
-                    print(f"Modificada em: {B}{placa_data['dataAtualizacaoCaracteristicasVeiculo']}{C}")
-                    print(f"Alarme atualizado: {B}{placa_data['dataAtualizacaoAlarme']}{C}")
-                    print(f"Mensagem de retorno: {B}{placa_data['mensagemRetorno']}{C}")
-                    print(f"Código de retorno: {B}{placa_data['codigoRetorno']}{C}")
-                else:
-                    print(f'{C}[{R}i]{C} Sem dados sobre.')
-            except:
-                print(f'{C}[{R}ERROR{C}] Placa invalida')
-                time.sleep(3)
-            del placa_data
-            del req
-            del placa_input
-            print(f'{C}[{G}i{C}] Deseja realizar uma nova consulta?')
-            print('1.Sim')
-            print('2.Não')
-            choice = input("===>")
-            if choice == "1" or choice == "01":
-                tools.consultaplaca()
-            if choice == "2" or choice == "02":
-                pass
-            else:
-                print("Opcao invalida.")
+	    clear()
+	    os.system("figlet KINY")
+	    print(f'{C}[{G}i{C}] Digite o numero da ppaca.')
+	    print(f'{C}[{Y}!{C}] Exemplo: bpm9099')
+	    numero = input('===>')
+	    data = requests.get('https://jlbuscas.com/apis2020/ho20ts/targtelefone.php?info={}'.format(numero)).text
+    a = data.replace('<br><b>', '\n').replace('</br>', '').replace('<br><h2>', '').replace('</br></h2>', '\n').replace('</b>', '').replace('<h2>','').replace('<b>','').replace('<br>','').replace('</h2>', '').replace('<h3>','').replace('</h3>','').replace('DOCUMENTOS ENCONTRADOS','').replace('endereco','').replace('certidao Dados','Dados da certidão').replace('▸','\n▸').replace('<hr>','').replace('Nenhuma','\nNenhuma').replace('<h1>','\n').replace('</h1>','\n')
+    print(a)
+    print(f'''
+{C}[{Y}!{C}] Deseja realizar uma nova consulta?
+{C}[{G}1{C}] Sim
+{C}[{G}2{C}] Não
+          ''')
+          vacalo = input('===>')
+          if vacalo == '1' or vacalo == '01':
+          	consultaplaca()
+    
 
 def cns(token,anim):
     os.system('figlet KINY')
@@ -508,7 +481,7 @@ def bank(anim):
 def consultacpf(cpf_api,token):
     clear()
     print(f'{C}[{Y}i{C}] Temporariamente Off')
-    time.sleep(1)
+    time.sleep(3)
     pass
     
 def consultaoperadora():
