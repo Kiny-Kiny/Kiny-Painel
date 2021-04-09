9bf6d0f927110778e8c9f47e3a3f66ee6c37c176
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-08 22:09:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -155,6 +155,14 @@ try:
         update = subprocess.check_output('git pull', shell=True)
         if 'Already up to date' not in update.decode():
             print(f'{C}[{Y}*{C}] {G}Atualizacao instalada!\n{C}[{Y}*{C}]Reiniciando o painel...{C}')
+            print("Loading:")
+            #animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+            animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+            for i in range(len(animation)):
+                time.sleep(0.2)
+                sys.stdout.write("\r" + animation[i % len(animation)])
+                sys.stdout.flush()
+            print("\n")
             time.sleep(5)
             subprocess.run('clear')
             restart()
