6dd2248f369859cd9c19fd25305607c18738b2c0
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-08 22:02:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -149,20 +149,20 @@ else:
 token = ["f01e0024a26baef3cc53a2ac208dd141","5072097263","25d800a8b8e8b99d77c809567aa291b8"]
 
 welcome_msg = ["Que a força esteja com você", "Bem vindo", "Você é um mito", "Okaerinasai", "Esta pessoa é muito boa no Websexo ->", "Você é um baitola"]
-    	try:
-    		if __name__ == '__main__':
-    			print(f'{C}[{Y}i{C}]{G} Checando por atualizacoes... {C}')
-    			update = subprocess.check_output('git pull', shell=True)
-    			if 'Already up to date' not in update.decode():
-    				print(f'{C}[{Y}*{C}] {G}Atualizacao instalada!\n{C}[{Y}*{C}]Reiniciando o painel...{C}')
-    				time.sleep(5)
-    				subprocess.run('clear')
-    				restart()
-    			else:
-    				print(f'{C}[{Y}i{C}]Nenhuma atualizacao disponivel.')
-    				time.sleep(2)
-    	except:
-    			pass
+try:
+    if __name__ == '__main__':
+        print(f'{C}[{Y}i{C}] {G} Checando por atualizacoes... {C}')
+        update = subprocess.check_output('git pull', shell=True)
+        if 'Already up to date' not in update.decode():
+            print(f'{C}[{Y}*{C}] {G}Atualizacao instalada!\n{C}[{Y}*{C}]Reiniciando o painel...{C}')
+            time.sleep(5)
+            subprocess.run('clear')
+            restart()
+        else:
+            print(f'{C}[{Y}i{C}] Nenhuma atualizacao disponivel.')
+            time.sleep(2)
+except:
+    pass
 
 if login == 1:
     tools.clear()
