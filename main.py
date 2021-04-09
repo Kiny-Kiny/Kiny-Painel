31de425043e483ac8e8f0563d87a020aa4981664
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-08 22:11:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ else:
 '''
 token = ["f01e0024a26baef3cc53a2ac208dd141","5072097263","25d800a8b8e8b99d77c809567aa291b8"]
 
-welcome_msg = ["Que a força esteja com você", "Bem vindo", "Você é um mito", "Okaerinasai", "Esta pessoa é muito boa no Websexo ->", "Você é um baitola"]
+welcome_msg = ["Que a força esteja com você", "Bem vindo", "Você é um mito", "Okaerinasai", "Esta pessoa é muito boa no Websexo ->", "Você é um baitola", "Você é corno"]
 try:
     if __name__ == '__main__':
         print(f'{C}[{Y}i{C}] {G} Checando por atualizacoes... {C}')
