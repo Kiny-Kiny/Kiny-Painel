1c62350fbde477190fdf8e4461c4cf2f98c8e750
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-10 23:12:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -305,13 +305,6 @@ while(Sair == False):
         	josuke = "InTheCourtOfTheCrimsonKing.mp3"
         	os.system("mpeg123" + josuke)
         	pass
-        if music == "05" or music == "5":
-        	os.system("clear")
-        	print(f'{C}{G}{result}{C}')
-        	print(f'{C}[{Y}i{C}] Aperte "Q" para parar a música.')
-        	tchola = "LoveTrain.mp3"
-        	os.system("mpeg123" + tchola)
-        	pass
         else:
         	pass
 
