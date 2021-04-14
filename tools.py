6c5a70c5c1c928cb98bd237a1b08505284418037
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-14 01:18:14 -0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -37,8 +37,10 @@ def clear():
 
 def chat():
 	os.system("pkg install irssi -y")
+	os.system("clear")
 	print(f'{C}{G}{result}{C}')
 	print(f'{C}[{Y}i{C}] TUTORIAL DE COMO ENTRAR NO CHAT')
+	print(f'{C}[{R}+{C}] RECOMENDO O USO DD VPN')
 	print()
 	print(f'{C}[{G}i{C}] /connect chat.freenode.net') 
 	print(f'{C}[{G}i{C}] /nick (seu nickname)')
