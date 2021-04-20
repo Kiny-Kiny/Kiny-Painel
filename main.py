6104ded38a8bd54e0fa9f4892c39a6c88ab34150
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-20 12:58:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ try:
     #from fordev.generator import people #Presente para algum dev que esteja lendo :p
 except:
     os.system('pip3 install requests phonenumbers urllib3 colorama bs4 html5lib argparse pytube pyfiglet')
-    restart()
+    pass
 
 result = pyfiglet.figlet_format("Kiny", font = "cosmic"  )
 
