e14563f6f437520e3f6510735a36fac5af64c65b
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-23 16:33:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def restart():
     os.execl(python, python, *sys.argv)
 
 import os,sys,time,base64, json, re,subprocess,webbrowser,platform
-os.system('pip3 install pyfiglet')
 try:
     import requests,platform,signal,atexit,argparse,random,hashlib,urllib3,html5lib,phonenumbers,json,tools, pyfiglet, cpf, nome
     from colorama import Fore, Style
@@ -37,6 +36,7 @@ try:
     import apicpf as b
     #from fordev.generator import people #Presente para algum dev que esteja lendo :p
 except:
+    os.system('pip3 install pyfiglet')
     os.system('python3 -m pip install --upgrade pip')
     os.system('pip3 install pyfiglet==0.7')
     os.system('pip3 install requests phonenumbers urllib3 colorama bs4 html5lib argparse pytube pyfiglet')
