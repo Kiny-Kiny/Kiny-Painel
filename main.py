7676c42fecd3a72e1674db005eb302f38925e7a1
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-04-23 15:16:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ except:
     os.system('python3 -m pip install --upgrade pip')
     os.system('pip3 install pyfiglet==0.7')
     os.system('pip3 install requests phonenumbers urllib3 colorama bs4 html5lib argparse pytube pyfiglet')
-
+    restart()
 
 result = pyfiglet.figlet_format("Kiny", font = "cosmic"  )
 
