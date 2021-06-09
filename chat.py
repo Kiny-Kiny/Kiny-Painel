f1304a84cd329afa74405e3fb0e9b58ad71c9606
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-09 02:57:39 -0300

    Update and rename chat to chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,17 @@
+global R, B, C, Y, G, RT, CY, CO
+CO = '\033[m'
+R = '\033[1;31m'
+B = '\033[1;34m'
+C = '\033[1;37m'
+CY = '\033[1;36m'
+Y = '\033[1;33m'
+G = '\033[1;32m'
+RT = '\033[;0m'
+
 import os
 import sys
 import pyfiglet
-
+result = pyfiglet.figlet_format("Kiny", font = "cosmic"  )
 def chat():
 	os.system("pkg install irssi -y")
 	os.system("clear")
