7205c475dc421cfcb4440bb905cb77044ece6e49
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-10 17:12:04 -0300

    Create numero.py

---
+++ data/numero.py
@@ -0,0 +1,33 @@
+import requests, phonenumbers
+from phonenumbers import geocoder, carrier
+from data import ui
+def numero():
+	Sair = False:
+		while(Sair == False):
+			if self == 1:
+				numero_input = ''
+			else:
+				numero_input = ui.input.dialog()
+				if len(numero_input) < 1:
+					ui.error_dialog('Digite algo para consultar')
+			
+			if '55' not in number_input:
+				ui.error_dialog('Digite o numero no formato 55219××××××××')
+				
+			pm = phonenumbers.parse(numero_input)
+			op = carrier.name_for_number(pm, 'pt-br')
+			es = geocoder.description_for_number(pm, 'pt-br')
+			
+			numero = f'''
+			Numero: {pm}
+			Estado: {es}
+			Operadora: {op}
+			'''
+			
+			choice = int(ui.dialog_choice(msg))
+			if choice == 1:
+				pass
+			elif choice == 2:
+				Sair = True
+			else:
+				ui.error_dialog()
