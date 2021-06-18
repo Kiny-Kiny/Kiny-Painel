d871af8ecb627d3bfb218e87c0d395512c1d7fe6
oporadokrl <71855447+oporadokrl@users.noreply.github.com>
2021-06-17 22:08:50 -0300

    Consulta por numero corrigida

--- data/numero.py
+++ data/numero.py
@@ -4,28 +4,29 @@ from data import ui
 def consultar():
 	Sair = False
 	while(Sair == False):
-				numero = ui.input_dialog()
-				if len(numero) < 1:
-					ui.error_dialog('Digite algo para consultar')
-					
-				if '+55' not in numero:
-					ui.error_dialog('Digite o numero no formato +55219××××××××')
-					Sair = True
-				
-				pm = phonenumbers.parse(numero)
-				op = carrier.name_for_number(pm, 'pt-br')
-				es = geocoder.description_for_number(pm, 'pt-br')
-				
-				msg = f'''
+		numero = ui.input_dialog()
+		if len(numero) < 1:
+			ui.error_dialog('Digite algo para consultar')
+		if len(numero) => 1 and len(numero) <= 12:
+			ui.error_dialog('Formato incorreto')
+		if '+55' not in numero:
+			ui.error_dialog('Digite o numero no formato +55219××××××××')
+			Sair = True
+		try:
+			pm = phonenumbers.parse(numero)
+			op = carrier.name_for_number(pm, 'pt-br')
+			es = geocoder.description_for_number(pm, 'pt-br')
+		except Exception as e:
+			ui.error_dialog('Ocorreu um erro: ',e)
+		msg = f'''
 Numero: {pm}
 Estado: {es}
 Operadora: {op}
 '''
-				
-				choice = int(ui.dialog_choice(msg))
-				if choice == 1:
-					pass
-				elif choice == 2:
-					Sair = True
-				else:
-					ui.error_dialog()
+		choice = int(ui.dialog_choice(msg))
+		if choice == 1:
+			pass
+		elif choice == 2:
+			Sair = True
+		else:
+			ui.error_dialog()
