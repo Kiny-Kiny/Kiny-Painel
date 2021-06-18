597ed73fee1cb582b2fd8079a71450473d75a09c
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-06-17 22:27:29 -0300

    Update numero.py

--- data/numero.py
+++ data/numero.py
@@ -7,25 +7,23 @@ def consultar():
 		numero = ui.input_dialog()
 		if len(numero) < 1:
 			ui.error_dialog('Digite algo para consultar')
-		elif len(numero) > 1 and	len(numero) <= 12:
+		if len(numero) > 1 and len(numero) < 12:
 			ui.error_dialog('Formato incorreto')
-		elif '+55' not in numero:
+		if '+55' not in numero:
 			ui.error_dialog('Digite o numero no formato +55219××××××××')
-			Sair = True
-		else:
-			try:
-				pm = phonenumbers.parse(numero)
-				op = carrier.name_for_number(pm, 'pt-br')
-				es = geocoder.description_for_number(pm, 'pt-br')
-			except Exception as e:
-				ui.error_dialog('Ocorreu um erro: ',e)
+			pass
+		try:
+			pm = phonenumbers.parse(numero)
+			op = carrier.name_for_number(pm, 'pt-br')
+			es = geocoder.description_for_number(pm, 'pt-br')
+		except Exception as e:
+			ui.error_dialog('Ocorreu um erro: ',e)
 		msg = f'''
 Numero: {pm}
 Estado: {es}
 Operadora: {op}
 '''
 		choice = int(ui.dialog_choice(msg))
-		del pm;del op;del es
 		if choice == 1:
 			pass
 		elif choice == 2:
