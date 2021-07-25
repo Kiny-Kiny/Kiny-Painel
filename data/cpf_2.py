ce0c325c764f66a4be67af57130e14430877bea0
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-25 16:35:37 -0300

    Update cpf_2.py

--- data/cpf_2.py
+++ data/cpf_2.py
@@ -1,33 +1,30 @@
-import requests,time
+import requests,re
 from data import ui
-# Pq tá olhando aqui? Veio roubar a API?
+# Api - https://github.com/gav1x/FullP
 def consultar():
     Sair = False
-    while(Sair == False):
-        cpf = str(ui.input_dialog()).replace('.','').replace('-','')
-        if len(cpf) != 11 or len(cpf) < 1:
-            msg = "error"
+    while(Sair==False):
+        cpf = ui.input_dialog()
+        url: str = 'https://consulta-cpf2.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/cpf.php'
+        params: str = {'consulta': cpf}
+        headers: str = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+        'x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730',
+        'x-rapidapi-host': 'consulta-cpf2.p.rapidapi.com'
+        }
+        req: str = requests.get(url=url, headers=headers, params=params)
+        ret = req.text
+        if 'A Consulta Esta Funcionando Normalmente' in ret:
+        	msg='A Consulta Esta Funcionando Normalmente, Porem O email Inserido Nao Foi Encontrado.'
         else:
-            api = requests.get('http://45.56.74.86:9547/'+cpf).json() # 00000000272
-            try:
-                msg=f"""
-Nome : {api['nomeCompleto']}
-CPF : {api['cpf']}
-Nome da Mãe : {api['nomeMae']}
-Aniversário : {api['dataNascimento']}
-Estado : {api['estado']}
-Cidade : {api['cidade']}
-CEP : {api['cep']}
-Logradouro : {api['logradouro']}
-Bairro : {api['bairro']}
-Número da Casa: {api['numero']}
-Complemento : {api['complemento']}"""
-            except(AttributeError):
-                msg = 'Erro no servidor'
-            choice = ui.dialog_choice(msg)
-            if choice == '1':
-                pass
-            elif choice == '2':
-                Sair = True
-            else:
-                ui.error_dialog()
+        	arquivo = open(f'cpf {cpf}.html', 'w', encoding='utf-8')
+        	arquivo.writelines(ret)
+        	arquivo.close()
+        	msg='Sua consulta foi salvo em um arquivo HTML'
+        choice = ui.dialog_choice(msg)
+        if choice == '1':
+        	pass
+        elif choice == '2':
+        	Sair=True
+        else:
+        	ui.error_dialog()
