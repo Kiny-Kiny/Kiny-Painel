ebbc794d80cfbd98e4384d50a9d08af04aa6efd5
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-25 12:19:09 -0300

    Update numero.py

--- data/numero.py
+++ data/numero.py
@@ -1,30 +1,32 @@
-import requests, phonenumbers
-from phonenumbers import geocoder, carrier
+import requests
 from data import ui
+#Api - https://github.com/gav1x/FullP
 def consultar():
 	Sair = False
 	while(Sair == False):
 		msg=''
 		numero = str(ui.input_dialog())
+		url: str = 'https://consulta-numero.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/numero.php'
+		params: str = {'consulta': numero}
+		headers: str = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+		'x-rapidapi-key':'0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730',
+		'x-rapidapi-host': 'consulta-numero.p.rapidapi.com'
+		}
+		req: str = requests.get(url=url, headers=headers, params=params)
+		ret = req.text
 		if len(str(numero)) < 1:
 			ui.error_dialog('Digite algo para consultar')
-		elif len(str(numero)) > 1 and	len(str(numero)) <= 12:
+		elif len(str(numero)) > 1 and	len(str(numero)) <= 11:
 			ui.error_dialog('Formato incorreto')
-		elif '+55' not in str(numero):
-			ui.error_dialog('Digite o numero no formato +55219××××××××')
-			Sair = True
 		else:
-			try:
-				pm = phonenumbers.parse(numero)
-				op = carrier.name_for_number(pm, 'pt-br')
-				es = geocoder.description_for_number(pm, 'pt-br')
-				msg = f'''
-Numero: {pm}
-Estado: {es}
-Operadora: {op}
-'''
-			except Exception as e:
-				ui.error_dialog('Ocorreu um erro: ',e)
+		      if 'A Consulta Esta Funcionando Normalmente' in ret:
+		      	ui.error_dialog('A Consulta Esta Funcionando Normalmente, Porem O Telefone Inserido Nao Foi Encontrado.')
+		      else:
+		      	arquivo = open(f'numero {numero}.html', 'w', encoding='utf-8')
+		      	arquivo.writelines(ret)
+		      	arquivo.close()
+		      	msg='Sua consulta foi salva em um arquivo HTML'
 		choice = int(ui.dialog_choice(msg))
 		if choice == 1:
 			pass
