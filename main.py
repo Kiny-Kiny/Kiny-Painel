be4f97d209203027fba56e9c3a2b170ca14786d1
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-25 23:44:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ try:
     os.system("apt install figlet curl -y")
 except:
     os.system("pacman -Sy figlet curl")
-
+os.system('termux-toast Seja bem-vindo ao Kiny-Painel! && termux-tts-speak Esta é uma simples tool de consulta de dados pessoais, aproveite!')
 Sair = False
 while(Sair == False):
     try:
