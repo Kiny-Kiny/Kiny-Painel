419c07456a0bb219dc51a326991937e25a2601e0
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 03:48:09 -0300

    Update nome.py

--- data/nome.py
+++ data/nome.py
@@ -5,22 +5,13 @@ def consultar():
     Sair = False
     while(Sair==False):
         nome = ui.input_dialog()
-        url: str = 'https://consulta-nome1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/nome.php'
-        params: str = {'consulta': nome}
-        headers: str = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
-        'x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730',
-        'x-rapidapi-host': 'consulta-nome1.p.rapidapi.com'
-        }
-        req: str = requests.get(url=url, headers=headers, params=params)
-        ret = req.text
-        if 'A Consulta Esta Funcionando Normalmente' in ret:
-        	msg='A consulta está funcionando normalmente, porém, o nome inserido não foi encontrado.'
+        r=requests.get(url='https://consulta-nome1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/nome.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-nome1.p.rapidapi.com'}, params={'consulta': nome}).text
+        if 'A Consulta Esta Funcionando Normalmente' in r:
+        	msg='A consulta está funcionando normalmente, porém, o CPF inserido não foi encontrado.'
+        if 'NOME: ' not in r:
+        	msg="Por favor, coloque um nome maior"
         else:
-        	arquivo = open(f'ConsultaNome_{nome}.html', 'w', encoding='utf-8')
-        	arquivo.writelines(ret)
-        	arquivo.close()
-        	msg='Sua consulta foi salva em um arquivo HTML'
+        	msg=r.replace('<br>', '\n').replace('<p>','')
         choice = ui.dialog_choice(msg)
         if choice == '1':
         	pass
