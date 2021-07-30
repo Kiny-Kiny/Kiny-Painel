1acc787ba9ce0f6d136da5994cea6445122c2004
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-30 12:07:24 -0300

    Update numero.py

--- data/numero.py
+++ data/numero.py
@@ -7,7 +7,8 @@ def consultar():
 		msg=''
 		numero = ui.input_dialog('Digite o número[Formato: DDD+Numero]: ')
 		data = requests.get(url='https://consulta-numero.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/numero.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'e01238c690msh74f20bdc84d5dcfp122562jsnc9921fa7c4c1','x-rapidapi-host': 'consulta-numero.p.rapidapi.com'}, params={'consulta': numero}).text
-		      msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
+		if 'A Consulta Esta Funcionando' in data:
+			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
 		else:
 			msg=data.replace('<p>', '').replace('<br>', '\n').replace('DDD:', 'DDD: ').replace('NOME:', 'Nome completo: ').replace('FONE:', 'Telefone: ').replace('INST:', 'Data de Compra: ').replace('PESSOA:', 'Pessoa: ').replace('CPF:', 'CPF: ').replace('PESSOA:', 'Pessoa: ')
 		choice = int(ui.dialog_choice(msg))
