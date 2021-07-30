eb67a80013d2d201cb84804793f5a6616d232432
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-30 11:47:55 -0300

    Update email.py

--- data/email.py
+++ data/email.py
@@ -5,8 +5,7 @@ def consultar():
     Sair = False
     while(Sair==False):
         email = ui.input_dialog()
-        r=requests.get(url='https://consulta-e-mail.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/email.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'9353b1d886msh73e7a0771d6600dp14ec4cjsn883fd8b4cf13','x-rapidapi-host': 'consulta-e-mail.p.rapidapi.com'}, params={'consulta': email}).text
-        if 'A Consulta Esta Funcionando Normalmente' in r:
+        r=requests.get(url='https://consulta-e-mail.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/email.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'e01238c690msh74f20bdc84d5dcfp122562jsnc9921fa7c4c1','x-rapidapi-host': 'consulta-e-mail.p.rapidapi.com'}, params={'consulta': email}).text
         	msg='A consulta está funcionando normalmente, porém, o Email inserido não foi encontrado.'
         else:
         	msg=r.replace('<br>', '\n').replace('<p>','')
