0cec5b57fd795d518df53b5648aa7ad4a204fab9
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-01 20:53:43 -0300

    Create cpf4.py

---
+++ data/cpf4.py
@@ -0,0 +1,21 @@
+import requests,re,os,sys
+from data import ui
+# Api - Lukazinn
+def consultar():
+    Sair = False
+    while(Sair==False):
+        cpf = ui.input_dialog()
+        r =(f'https://lukazinnapi.000webhostapp.com/api/consulta/cpf/cpf2.php?cpf={cpf}').text
+        if 'A Consulta' in cpf:
+        	msg='A consulta está funcionando normalmente, porém o CPF não foi encontrado.'
+        else:
+        	# Preguiça é o nome
+        	msg=a.replace('''"''', '').replace(',','\n').replace('}', '').replace('{', '')
+        choice = ui.dialog_choice(msg)
+        if choice == '1':
+        	pass
+        elif choice == '2':
+        	Sair=True
+        else:
+        	ui.error_dialog()
+
