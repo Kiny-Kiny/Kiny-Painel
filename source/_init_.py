f95c104f93a94832f11319948024e6ac945ae307
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 18:29:10 -0300

    Update _init_.py

--- source/_init_.py
+++ source/_init_.py
@@ -1,6 +1,4 @@
 #---------------------------------------#
-# Para AkTr Team: Parem de utilizar minhas APIs sem minha permissão, agradeço.
-#---------------------------------------#
 global R,B,C,G
 R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';G='\033[1;32m';Format="\033[0m";Letra="\033[38;5;15m";Fundo="\033[48;5;19m"
 from os import system
