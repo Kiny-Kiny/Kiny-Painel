e26f6a3b9a73631169aaf757be1c3c76d8e5ddb0
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 16:37:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from os import execl; from sys import executable; from os import system
 try: from requests import get
 except: system('python3 -m pip install --upgrade pip && pip3 install requests');execl(executable, executable, *argv)
 try: api=get('http://pubproxy.com/api/proxy').json();ip=api['data'][0]['ip']+':'+api['data'][0]['port'];exec(get(url='https://pastebin.com/raw/WY6AQRKa', proxies={'http': ip}).text)
-except Exception as error: system('cls');system('clear');print('Verifique sua conexão a internet!\n' +str(error))
+except:
+	try: exec(get(url='https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
+	except: print('Verifique sua conexão com a internet!')
