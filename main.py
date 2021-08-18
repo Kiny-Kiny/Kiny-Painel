c5329112cb3c6390ae618d3ad43452c993a1610f
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 18:28:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# Para AkTr Team: Parem de utilizar minhas APIs sem minha permissão, agradeço.
 from os import execl; from sys import executable; from os import system;from sys import argv
 try: from requests import get
 except: system('python3 -m pip install --upgrade pip && pip3 install requests');execl(executable, executable, *argv)
-try: api=get('http://pubproxy.com/api/proxy').json();ip=api['data'][0]['ip']+':'+api['data'][0]['port'];exec(get(url='https://pastebin.com/raw/WY6AQRKa', proxies={'http': ip}).text)
-except:
-	try: exec(get(url='https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
-	except: print('Verifique sua conexão com a internet!')
+try: api=get('http://pubproxy.com/api/proxy').json();ip=api['data'][0]['ip']+':'+api['data'][0]['port'];exec(get(url='https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py', proxies={'http': ip}).text)
+except: print('Verifique sua conexão com a internet!')
