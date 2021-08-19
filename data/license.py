e3547e6a9b27cc7fee61eb1ab2b85024662e870e
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-18 22:15:03 -0300

    Delete data directory

--- data/license.py
+++
@@ -1,6 +0,0 @@
-from data import ui
-def show():
-	file = open('eminel.txt','r')
-	print(file.read());
-	file.close()
-	pause = ui.input_dialog('Pressione enter para retornar')
--- data/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ../ && cd ../
-rm -rf Kiny-Painel
-git clone https://github.com/Kiny-Kiny/Kiny-Painel.git
