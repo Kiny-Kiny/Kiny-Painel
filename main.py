5413651dd44ff57bf8919c4565e68ab3eb5d527c
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-23 18:07:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#netin baitola
 from os import execl; from sys import executable; from os import system;from sys import argv
 try: from requests import get;from TerminalButtons import *
 except: system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons');execl(executable, executable, *argv)
