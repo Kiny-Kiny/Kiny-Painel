fae84c4b0060079c467846c69114857d3eee37ed
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-08-23 15:47:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from os import execl; from sys import executable; from os import system;from sys import argv
 try: from requests import get;from TerminalButtons import *
 except: system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons');execl(executable, executable, *argv)
-try: exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
+try: get('https://dualityapi.xyz', 'https://dualitybuscas.com');exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
 except: print('Verifique sua conexão com a internet!')
