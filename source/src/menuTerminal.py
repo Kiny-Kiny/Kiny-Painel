99872f096e5f99c8a463648b3b743e359155a288
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2022-07-10 22:13:31 -0300

    Update menuTerminal.py

--- source/src/menuTerminal.py
+++ source/src/menuTerminal.py
@@ -55,7 +55,7 @@ def req(api_req) -> str: return loads(get(api_req).text)
 def clear(clean) -> None: return system(clean)
 #######################
 def cpf() -> str:
-	result=loads(get(r'http://api.lkzn.tk/api/cpfSimples.php?token=9c06b7c4-627e-4b66-8837-f1b82c2c3854&cpf='+input('%s%s%s\n%s>%s Digite o CPF : '%(B,logo,C,G,C)),verify=False).text)
+	result=loads(get(r'http://api.lkzn.tk/cpfSimples.php?token=9c06b7c4-627e-4b66-8837-f1b82c2c3854&cpf='+input('%s%s%s\n%s>%s Digite o CPF : '%(B,logo,C,G,C)),verify=False).text)
 	clear(clean)
 	ban='%s%s%s\n'%(B,logo,C)
 	try: return ban+'[ %sNome%s : %s ]\n[ %sCPF%s : %s ]\n[ %sAno de Nascimento%s : %s ]\n[ %sSexo %s: %s ]'%(G,C,result['msg']['nome'],G,C,result['msg']['cpf'],G,C,result['msg']['nascimento'],G,C,result['msg']['sexo'])
@@ -89,7 +89,7 @@ def placa() -> str:
 	return msg
 
 def nome() -> str:
-	result='http://api.lkzn.tk/api/nome.php?token=9c06b7c4-627e-4b66-8837-f1b82c2c3854&nome=%s'%input('%s%s%s\n%s>%s Digite o nome: '%(B,logo,C,G,C))
+	result='http://api.lkzn.tk/nome.php?token=9c06b7c4-627e-4b66-8837-f1b82c2c3854&nome=%s'%input('%s%s%s\n%s>%s Digite o nome: '%(B,logo,C,G,C))
 	result=req(result)
 	msg=''
 	try:
